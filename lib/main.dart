1a179f4027f1409e356b7b49034d4f9e578aa710
samiraliyev573 <aliyev7137@gmail.com>
2021-01-31 02:10:21 -0800

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import './screens/loginscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +17,13 @@ class MyApp extends StatelessWidget {
       title: 'McHacks2021',
       theme: ThemeData(fontFamily: "Roboto-Light", brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: AnimatedSplashScreen(
+        splash: Image.asset('assets/images/logo.png'),
+        nextScreen: LoginScreen(),
+        splashTransition: SplashTransition.slideTransition,
+        backgroundColor: Color.fromRGBO(40, 40, 50, 1),
+        duration: 3000,
+      ),
     );
   }
 }
